318b18088c5a31a35ffc52dd58fc45c7cc10a9bd
y9vad9 <y9vad9@gmail.com>
2024-04-12 19:56:11 +0200

    init: initial preparation (README, base modules, etc.).

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "restaurant-coursework"
+
+include(":server", ":api", ":desktop")
